851cc4fad22aa8487f33e7c48bca8de588e46537
haebin.lee <luckylucy0717@gmail.com>
2023-11-01 20:58:53 -0700

    CreateMeetingForm update

--- src/components/CreateMeetingForm.js
+++ src/components/CreateMeetingForm.js
@@ -1,25 +1,21 @@
-import React, { Component, useEffect } from "react";
+import React, { useState } from "react";
 import { Button, Form, Row, Col, Table } from "react-bootstrap";
 import { createMeeting, addAttendees } from "../services/api";
 import styles from "../assets/css/CreateSpaceForm.module.css";
-import {qrGenerator} from './QRCodeGenerator';
+import { qrGenerator } from "./QRCodeGenerator";
 
-class CreateMeetingForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      description: "",
-      attendees: [],
+export default function CreateMeetingForm() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [eventAt, setEventAt] = useState("");
+  const [registerNow, setRegisterNow] = useState(false);
+  const [attendees, setAttendees] = useState([]);
+  const [attendeeId, setAttendeeId] = useState(0);
+  const [attendeeName, setAttendeeName] = useState("");
+  const [attendeeEmail, setAttendeeEmail] = useState("");
+  const [attendeePhone, setAttendeePhone] = useState("");
 
-      attendeeId: 0,
-      attendeeName: "",
-      attendeeEmail: "",
-      attendeePhone: "",
-    };
-  }
-
-  generateQRAddr = async (validationUrl) => {
+  async function generateQRAddr(validationUrl) {
     try {
       const qrDataURL = await qrGenerator(validationUrl);
       return qrDataURL;
@@ -27,27 +23,42 @@ class CreateMeetingForm extends Component {
       console.error(error);
       throw error;
     }
-  };
+  }
 
-  handleChange = (event) => {
+function handleChange(event) {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+    switch (name) {
+      case "name":
+        setName(value);
+        break;
+      case "description":
+        setDescription(value);
+        break;
+      case "eventAt":
+        setEventAt(value);
+        break;
+      default:
+        break;
+    }
+  }
+  function handleChecked(event) {
+    setRegisterNow(event.target.checked);
+  }
 
-  handleSubmit = async (event) => {
+  async function handleSubmit(event) {
     event.preventDefault();
-    const { name, description, attendees } = this.state;
-    const validationUrl = this.getValidationUrl();
+    const validationUrl = getValidationUrl();
     try {
-      const qrUrl = await this.generateQRAddr(validationUrl);
+      const qrUrl = await generateQRAddr(validationUrl);
       const body = {
         name: name,
         description: description,
         qrUrl: qrUrl,
         validationUrl: validationUrl,
-        registerNow: true,
+        registerNow: registerNow,
+        eventAt: eventAt,
       };
-      console.log("body", body);
+      console.log('body', body)
 
       createMeeting(body)
         .then((res) => {
@@ -64,16 +75,14 @@ class CreateMeetingForm extends Component {
     } catch (error) {
       console.log(error);
     }
-  };
+  }
 
-  getValidationUrl = () => {
+  function getValidationUrl() {
     const apiUrl = process.env.REACT_APP_API_URL;
     return apiUrl + "/" + Math.random().toString(36).slice(2, 20);
-  };
+  }
 
-  handleAttendeeAdd = (event) => {
-    const { attendeeId, attendeeName, attendeeEmail, attendeePhone } =
-      this.state;
+  const handleAttendeeAdd = () => {
     if (attendeeName && attendeeEmail && attendeePhone) {
       const newAttendee = {
         id: attendeeId,
@@ -81,152 +90,167 @@ class CreateMeetingForm extends Component {
         email: attendeeEmail,
         phone: attendeePhone,
       };
-
-      // Add the new attendee to the attendees array
-      this.setState((prevState) => ({
-        attendees: [...prevState.attendees, newAttendee],
-        attendeeId: attendeeId + 1,
-        attendeeName: "",
-        attendeeEmail: "",
-        attendeePhone: "",
-      }));
+      setAttendees([...attendees, newAttendee]);
+      setAttendeeId(attendeeId + 1);
+      setAttendeeName("");
+      setAttendeeEmail("");
+      setAttendeePhone("");
     }
   };
 
-  handleAttendeeChange = (e) => {
+  function handleAttendeeChange(event) {
     this.setState({
-      [e.target.name]: e.target.value,
+      [event.target.name]: event.target.value,
     });
-  };
-
-  render() {
-    console.log("rendering");
-    const { name, description, attendees } = this.state;
-
-    return (
-      <div className={styles.container}>
-        <form>
-          <div className="mb-3">
-            <label htmlFor="exampleName" className="form-label">
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              className="form-control"
-              id="formControlName"
-              placeholder="Enter your meet-up name"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="exampleDescription" className="form-label">
-              Description
-            </label>
-            <textarea
-              type="textarea"
-              name="description"
-              value={description}
-              onChange={this.handleChange}
-              className="form-control"
-              id="formControlDescription"
-            />
-          </div>
-          <div className="d-grid justify-content-md-end">
-            <button
-              type="submit"
-              className="btn btn-primary"
-              onClick={this.handleSubmit}
-            >
-              Save
-            </button>
-          </div>
-        </form>
-
-        {/* <Form>
-          <Form.Group className="mb-3" controlId="group_name">
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              placeholder="Enter your meet-up name"
-            />
-            <Form.Text className="text-muted" required>
-              Enter your meet-up name
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              as="textarea"
-              name="description"
-              value={description}
-              onChange={this.handleChange}
-              rows={3}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label>Attendee</Form.Label>
-            <Table striped>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Phone</th>
-                  <th>status</th>
-                  <th>Edit</th>
-                </tr>
-              </thead>
-              <tbody>
-                <Attendees attendees={attendees} />
-              </tbody>
-            </Table>
-          </Form.Group>
-          <Form.Group>
-            <Row>
-              <Col>
-                <Form.Control
-                  name="attendeeName"
-                  placeholder="Name"
-                  value={this.state.attendeeName}
-                  onChange={this.handleAttendeeChange}
-                />
-              </Col>
-              <Col>
-                <Form.Control
-                  name="attendeeEmail"
-                  placeholder="Email"
-                  value={this.state.attendeeEmail}
-                  onChange={this.handleAttendeeChange}
-                />
-              </Col>
-              <Col>
-                <Form.Control
-                  name="attendeePhone"
-                  placeholder="phone"
-                  value={this.state.attendeePhone}
-                  onChange={this.handleAttendeeChange}
-                />
-              </Col>
-              <Col>
-                <Button variant="success" onClick={this.handleAttendeeAdd}>
-                  Add
-                </Button>
-              </Col>
-            </Row>
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={this.handleSubmit}>
-            Save
-          </Button>
-        </Form> */}
-      </div>
-    );
   }
+
+  return (
+    <div className={styles.container}>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="exampleName" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            className="form-control"
+            id="formControlName"
+            placeholder="Enter your meet-up name"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleDescription" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="textarea"
+            name="description"
+            value={description}
+            onChange={handleChange}
+            className="form-control"
+            id="formControlDescription"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleDate" className="form-label">
+            Event Date
+          </label>
+          <input
+            type="datetime-local"
+            name="eventAt"
+            value={eventAt}
+            onChange={handleChange}
+            className="form-control"
+            id="formDate"
+          />
+        </div>
+        <div className="form-check form-switch">
+          <label className="form-check-label">Register on the spot</label>
+          <input 
+            type="checkbox"
+            name="registerNow"
+            value={registerNow}
+            onChange={handleChecked}
+            className="form-check-input" 
+            role="switch" 
+            id="formToggle"/>
+        </div>
+        <div className="d-grid justify-content-md-end">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          >
+            Save
+          </button>
+        </div>
+      </form>
+
+      {/* <Form>
+        <Form.Group className="mb-3" controlId="group_name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            placeholder="Enter your meet-up name"
+          />
+          <Form.Text className="text-muted" required>
+            Enter your meet-up name
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="description"
+            value={description}
+            onChange={this.handleChange}
+            rows={3}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Attendee</Form.Label>
+          <Table striped>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>status</th>
+                <th>Edit</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Attendees attendees={attendees} />
+            </tbody>
+          </Table>
+        </Form.Group>
+        <Form.Group>
+          <Row>
+            <Col>
+              <Form.Control
+                name="attendeeName"
+                placeholder="Name"
+                value={this.state.attendeeName}
+                onChange={this.handleAttendeeChange}
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                name="attendeeEmail"
+                placeholder="Email"
+                value={this.state.attendeeEmail}
+                onChange={this.handleAttendeeChange}
+              />
+            </Col>
+            <Col>
+              <Form.Control
+                name="attendeePhone"
+                placeholder="phone"
+                value={this.state.attendeePhone}
+                onChange={this.handleAttendeeChange}
+              />
+            </Col>
+            <Col>
+              <Button variant="success" onClick={this.handleAttendeeAdd}>
+                Add
+              </Button>
+            </Col>
+          </Row>
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+          Save
+        </Button>
+      </Form> */}
+    </div>
+  );
 }
 
 function Attendees({ attendees }) {
@@ -257,4 +281,3 @@ const attendeeData = [
   { id: 2, name: "Lucy", email: "hblee8080@gmail.com", phone: "6267863192" },
   { id: 3, name: "Lucy", email: "hblee8080@gmail.com", phone: "6267863192" },
 ];
-export default CreateMeetingForm;
